b32b58d3dbd6cfe31550aa614cd74a9bb36ec87f
SaadAldulaijan <SaadAldulaijan@users.noreply.github.com>
2020-11-05 21:14:35 +0300

    first commit

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "hello world";
+
+$testvar = "test";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p style="color:red;"><?php echo $testvar ?></p>
+</body>
+</html>

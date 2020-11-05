2122aff881366fe815c3dc7903def3eb11e14b87
SaadAldulaijan <SaadAldulaijan@users.noreply.github.com>
2020-11-05 21:20:15 +0300

    test

--- pages/main.php
+++ pages/main.php
@@ -13,9 +13,12 @@ $testvar = "test";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
 <body>
     <p style="color:red;"><?php echo $testvar ?></p>
+
+    <script src="./js/scripts.js"></script>
 </body>
 </html>
